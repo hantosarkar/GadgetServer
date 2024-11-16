86df63a751f933cc44020a45a2e6fd2c3024eeef
Administrator <abdusshobhan.4444@gmail.com>
2024-11-16 20:35:08 +0600

    add user in database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, Collection } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.port || 3000;
@@ -13,18 +13,30 @@ app.use(express.json())
 // mongodb 
 const url = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.kssadct.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-const client = new MongoClient(url , {
+const client = new MongoClient(url, {
     serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
     }
-  });
+});
+
+const Db_gadgetShop = client.db('GadgetShop').collection('user');
+
 
 const dbConnect = async () => {
     try {
         await client.connect();
         console.log('Database use is Connect');
+
+        app.post('/user', async (req, res) => {
+            const user = req.body
+            console.log(user);
+            const result = await Db_gadgetShop.insertOne(user);
+            res.status(200).send(result);
+        })
+
+
     } catch (error) {
         console.log(error.name, error.message)
     }
@@ -36,6 +48,8 @@ dbConnect();
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
