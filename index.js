94ac33085d27b43e466b67a497a8d7af0a2c8038
Administrator <abdusshobhan.4444@gmail.com>
2024-11-22 21:41:55 +0600

    vercel json

--- index.js
+++ index.js
@@ -43,6 +43,18 @@ const dbConnect = async () => {
             res.send({ token });
         })
 
+        app.get('/user:user', async (req, res) => {
+            const email = req.params?.user;
+            if (email) {
+                const query = { email: email }
+                const result = await Db_gadgetShop_User.findOne(query, { projection: { role: 1 , wishlist : 1 } });
+                res.send(result);
+            } else {
+                res.send({ message: "Login First" });
+            }
+        })
+
+
 
         app.post('/user', async (req, res) => {
             const user = req.body;
@@ -93,6 +105,15 @@ const dbConnect = async () => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await Db_gadgetShop_Product.findOne(query);
+            res.send(result);
+        })
+        app.patch('/wishlist', async (req, res) => {
+            const { id, user } = req.body;
+            const query = { email:user?.email}
+            const result = await Db_gadgetShop_User.updateOne(
+                query,
+                { $addToSet: { wishlist: id } }
+            );
             res.send(result);
         })
 
